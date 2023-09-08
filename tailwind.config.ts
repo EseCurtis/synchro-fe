a8d28cdc7f90bfa461302c798eeab1f3023a0a0c
yhoungdev <yhoungdev@gmail.com>
2023-09-08 18:18:15 +0100

    synco

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,8 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        primary: 'red'
+        primary: '#1A202C',
+
       }
     },
   },
