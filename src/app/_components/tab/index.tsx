8f729afab1cb4cf4fe792ff86978f71a32bbe8d5
yhoungdev <yhoungdev@gmail.com>
2023-09-16 14:26:10 +0100

    chore ( componenet ) : created tab component

---
+++ src/app/_components/tab/index.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import React, { FC, ReactNode, useState } from 'react';
+
+interface ITabData {
+  header: string;
+  route: ReactNode | string;
+}
+
+interface IpropsData {
+  data: ITabData[];
+}
+
+const TabComponent: FC<IpropsData> = ({ data }: IpropsData) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <div>
+      <div className='flex gap-[5em]'>
+        {data.map((tab, index) => (
+          <button
+            key={index}
+            onClick={() => handleTabClick(index)}
+            className={`tab-button ${activeTab === index ? 'active-tab' : ''}`}
+          >
+            {tab.header}
+          </button>
+        ))}
+      </div>
+      <div className='tab-content my-10'>
+        {data.map((tab, index) => (
+          <div
+            key={index}
+            className={`tab-pane ${activeTab !== index ? 'hidden' : ''}`}
+          >
+            {tab.route}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TabComponent;
--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -1,8 +1,29 @@
+import TabComponent from '@/app/_components/tab';
 import DashboardLayout from '@/app/layouts/dashboardLayout';
 import React from 'react';
 
+const data = [
+  {
+    header: 'Home',
+    route: 'Saved',
+  },
+  {
+    header: 'Profile',
+    route: 'Profile',
+  },
+  {
+    header: 'Settings',
+    route: 'Settings',
+  },
+  // Add more objects here as needed
+];
+
 const Events = () => {
-  return <DashboardLayout title='Events'>This is the sidebar</DashboardLayout>;
+  return (
+    <DashboardLayout title='Events'>
+      <TabComponent data={data} />
+    </DashboardLayout>
+  );
 };
 
 export default Events;
