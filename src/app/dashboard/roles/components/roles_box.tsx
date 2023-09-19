58b1b459c710fafb8826a909a8fd90b861c3c70a
yhoungdev <yhoungdev@gmail.com>
2023-09-19 23:17:00 +0100

    updated

--- src/app/dashboard/roles/components/roles_box.tsx
+++ src/app/dashboard/roles/components/roles_box.tsx
@@ -1,11 +1,28 @@
 import { FC } from 'react';
-
+import { MdMoreHoriz } from 'react-icons/md';
 interface IPropsRoles {
   role: string;
   content: string;
 }
 
+const styles = {
+  border: '1.5px solid #EDEFF5 ',
+  height: '155px',
+  padding: '10px 15px',
+};
+
 const RolesComponent: FC<IPropsRoles> = ({ role, content }) => {
-  return <div>lorel</div>;
+  return (
+    <div className='w-[260px] ' style={styles}>
+      <div className='flex justify-between items-center'>
+        <h3 className='font-md  text-[18px] '>Admin</h3>
+        <MdMoreHoriz />
+      </div>
+
+      <p className='text my-4'>
+        Lorem ipsum dolor sit amet consectetur. Eu vestibulum tempor.
+      </p>
+    </div>
+  );
 };
 export default RolesComponent;
--- src/app/dashboard/roles/views/roles.tsx
+++ src/app/dashboard/roles/views/roles.tsx
@@ -1,10 +1,12 @@
 import RolesAndPermission from '@/app/_components/no_data/roles_and_permission';
 import React from 'react';
+import RolesComponent from '../components/roles_box';
 
 const RolesPage = () => {
   return (
     <div>
       <RolesAndPermission />
+      <RolesComponent role='Admin' content='Lorem' />
     </div>
   );
 };
