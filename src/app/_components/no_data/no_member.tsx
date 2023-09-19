8add3952101952bdbff409af7385ad065f5def10
yhoungdev <yhoungdev@gmail.com>
2023-09-19 18:54:25 +0100

    chore: ( UI) updated color for no data

--- src/app/_components/no_data/no_member.tsx
+++ src/app/_components/no_data/no_member.tsx
@@ -9,8 +9,8 @@ const NoRolesMember = () => {
         <div className='my-5'>{NoRoleSvg}</div>
       </center>
 
-      <h3>Add a Member</h3>
-      <p className='text-text_primary text-center'>
+      <h3 className='font-bold'>Add a Member</h3>
+      <p className='text-second_primary_text text-center'>
         Click on the button below to add a member to your backoffice.
       </p>
       <Button>Add Role</Button>
--- src/app/_components/no_data/roles_and_permission.tsx
+++ src/app/_components/no_data/roles_and_permission.tsx
@@ -9,8 +9,8 @@ const RolesAndPermission = () => {
         <div className='my-5'>{NoRoleSvg}</div>
       </center>
 
-      <h3>Add a role</h3>
-      <p className='text-text_primary text-center'>
+      <h3 className='font-bold'>Add a role</h3>
+      <p className='text-second_primary_text text-center'>
         Click on the button to set up roles for your backoffice
       </p>
       <Button>Add Role</Button>
