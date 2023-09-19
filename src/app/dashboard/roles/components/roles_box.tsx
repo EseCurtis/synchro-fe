8c509fca1f486905938b7f94a3a0e8728ef049b9
yhoungdev <yhoungdev@gmail.com>
2023-09-19 18:57:44 +0100

    feat: added component for roles and members

---
+++ src/app/dashboard/roles/components/roles_box.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+interface IPropsRoles {
+  role: string;
+  content: string;
+}
+
+const RolesComponent: FC<IPropsRoles> = ({ role, content }) => {
+  return <div>lorel</div>;
+};
+export default RolesComponent;
