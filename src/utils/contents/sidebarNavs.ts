52c4de1404b3861be9ac4a8e5b9c4c7f82af9286
Ese Curtis <curtisese52@gmail.com>
2023-09-21 17:23:02 +0100

    feat: added active icons

--- src/utils/contents/sidebarNavs.ts
+++ src/utils/contents/sidebarNavs.ts
@@ -3,70 +3,80 @@ export const SidebarNavs = [
         title: 'Dashboard',
         path: '/dashboard',
         icon: '',
-        img: '/images/icons/sidebar/home_active.svg',
+        img: '/images/icons/sidebar/home.svg',
         active: '/images/icons/sidebar/home_active.svg'
     },
     {
         title: 'Users',
         path: '/dashboard/users',
         icon: '',
-        img: '/images/icons/sidebar/user.svg'
+        img: '/images/icons/sidebar/user.svg',
+        active: '/images/icons/sidebar/user_active.svg'
         
     },
     {
         title: 'events',
         path: '/dashboard/events',
         icon: '',
-         img: '/images/icons/sidebar/coupon.svg'
+         img: '/images/icons/sidebar/coupon.svg',
+         active: '/images/icons/sidebar/coupon_active.svg'
     },
     {
         title: 'kyc',
         path: '/dashboard/kyc',
         icon: '',
-        img: '/images/icons/sidebar/kyc.svg'
+        img: '/images/icons/sidebar/kyc.svg',
+        active: '/images/icons/sidebar/kyc_active.svg'
     },
     {
         title: 'Services',
         path: '/dashboard/services',
         icon: '',
-         img: '/images/icons/sidebar/user-dollar.svg'
+         img: '/images/icons/sidebar/user-dollar.svg',
+         active: '/images/icons/sidebar/user-dollar_active.svg'
     },
     {
         title: 'Support',
         path: '/dashboard/supports',
         icon: '',
-         img: '/images/icons/sidebar/support.svg'
+         img: '/images/icons/sidebar/support.svg',
+         active: '/images/icons/sidebar/support_active.svg'
     },
     {
         title: 'Report Issues',
         path: '/dashboard/reports',
         icon: '',
-        img: '/images/icons/sidebar/report.svg'
+        img: '/images/icons/sidebar/report.svg',
+        active: '/images/icons/sidebar/report_active.svg'
     },
     {
         title: 'Roles & Permissions',
         path: '/dashboard/roles',
         icon: '',
-         img: '/images/icons/sidebar/roles.svg'
+         img: '/images/icons/sidebar/roles.svg',
+         active: '/images/icons/sidebar/roles_active.svg'
     },
  
     {
         title: 'Audith Trail',
         path: '/dashboard/audits',
         icon: '',
-         img: '/images/icons/sidebar/audit.svg'
+         img: '/images/icons/sidebar/audit.svg',
+         active: '/images/icons/sidebar/audit_active.svg'
     },
     {
         title: 'FAQ & Notifications',
         path: '/dashboard/faqs',
         icon: '',
-        img: '/images/icons/sidebar/faq.svg'
+        img: '/images/icons/sidebar/faq.svg',
+        active: '/images/icons/sidebar/faq_active.svg'
     },
     {
         title: 'Settings',
         path: '/dashboard/settings',
         icon: '',
-        img: '/images/icons/sidebar/settings.svg'
+        img: '/images/icons/sidebar/settings.svg',
+        active: '/images/icons/sidebar/settings_active.svg'
     },
     
 ]
